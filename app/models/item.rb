8d752e18d04004772dfc637b101bea9554288dc9
taka826 <on85.ohno.takamasa@gmail.com>
2020-10-22 16:03:44 +0900

    Conditionの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,19 @@
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-
   belongs_to :user
   has_one_attached :image
   with_options presence: true do
     validates :name
     validates :description
-    validates :category
-    validates :condition_id
+    validates :category_id
+    validates :condition
     validates :cost_id
     validates :area_id
     validates :days_id
     validates :price
     validates :image
   end
-  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :condition
 end
--- spec/factories/conditions.rb
+++ spec/factories/conditions.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :category do
+  factory :condition do
     
   end
 end
