e462cfa189cd0616d381e0457c89306b9b384e43
taka826 <on85.ohno.takamasa@gmail.com>
2020-10-22 18:02:52 +0900

    ログインしていない場合はログイン画面に遷移を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :authenticate_user!, except: :index
 
   def index
     @items = Item.all
