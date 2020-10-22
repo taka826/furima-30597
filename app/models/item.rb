1ae04aa52d297c3e488bf3f582dd639fbb91c480
taka826 <on85.ohno.takamasa@gmail.com>
2020-10-22 16:47:27 +0900

    Categoryの修正実装、Costの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,16 +4,20 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
+    validates :category
     validates :condition
-    validates :cost_id
+    validates :cost
     validates :area_id
     validates :days_id
     validates :price
     validates :image
   end
   validates :condition_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 }
+  validates :cost_id, numericality: { other_than: 1 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
+  belongs_to_active_hash :category
+  belongs_to_active_hash :cost
 end
