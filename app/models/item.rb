3d85b812771d01a5c082bc64c418e1619204f0a0
taka826 <on85.ohno.takamasa@gmail.com>
2020-10-22 16:59:59 +0900

    Areaの実装、Dayの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,17 +7,21 @@ class Item < ApplicationRecord
     validates :category
     validates :condition
     validates :cost
-    validates :area_id
-    validates :days_id
+    validates :area
+    validates :days
     validates :price
     validates :image
   end
   validates :condition_id, numericality: { other_than: 1 }
   validates :category_id, numericality: { other_than: 1 }
   validates :cost_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 0 }
+  validates :days_id, numericality: { other_than: 1 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
   belongs_to_active_hash :category
   belongs_to_active_hash :cost
+  belongs_to_active_hash :area
+  belongs_to_active_hash :days
 end
