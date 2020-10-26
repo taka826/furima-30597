b7e51759b6fed43ba26a731c2a117fbcc0b01969
taka826 <on85.ohno.takamasa@gmail.com>
2020-10-26 19:18:10 +0900

    エラーメッセージ実装、非ログイン時の挙動実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,9 +25,18 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
