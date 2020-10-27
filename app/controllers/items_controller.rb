2afc1b3823cf0568c14587de7747042787e46bef
taka826 <on85.ohno.takamasa@gmail.com>
2020-10-27 11:50:14 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :edit_warp_prohibited, only:[:edit]
 
   def index
@@ -39,6 +39,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
