7ff4b820a2e501b1ff3dde01ce79cb26d9abac3a
taka826 <on85.ohno.takamasa@gmail.com>
2020-10-27 19:05:58 +0900

    ルーティングのネスト、購入価格アイテム名表記実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,29 @@
 class OrdersController < ApplicationController
 
   def index
-    @order = Order.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def new
+    @user = user.new
   end
 
   def create
-    @order = Order.new
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
+    @user = User.create(order_params)
+    if @user.save
+      redirect_to action: :index
     else
-      render 'index'
+      render action: :new
     end
   end
 
   private
 
+  def order_params
+    params.require(:user).permit(:nickname, :email, :encrypted_password, :first_name, :last_name, :first_kana, :last_kana, :birthday)
+  end
+  
+  def item_params
+    params.require(:item).permit(:content, :image, :name, :description, :category_id, :condition_id, :cost_id, :area_id, :day_id, :price, :user).merge(user_id: current_user.id)
+  end
 end
