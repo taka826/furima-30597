a89b4599e57ba8dae1abd0dbcb077e33ad743472
taka826 <on85.ohno.takamasa@gmail.com>
2020-10-28 19:45:09 +0900

    Formオブジェクトパターンのテストコード実装

--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -3,12 +3,14 @@ class UserItem
   attr_accessor :postal_code, :area_id, :city, :house_number, :phone, :build_name, :order_id
  #addressから切り取ったバリデーション
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "Postal code is invalid. Include hyphen(-)"}
     validates :area_id
     validates :city
     validates :house_number
-    validates :phone
+    validates :phone, format: { with: /\A[0-9]{3}[0-9]{4}[0-9]{4}\z/, message: "Phone number is invalid. Include hyphen(-)"}
   end
+
+  validates :area_id, numericality: { other_than: 0 }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
